853c81e081dbb9f7f8e38d862a3970a91761d469
Ronny Rook <info@ronnyrook.nl>
2023-10-26 12:02:11 +0200

    Use router state to save location on inital load

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { DetailView } from './DetailView'
 import { AnimatePresence } from 'framer-motion'
 import { SetLocationProps } from 'pages'
+import { useRouter } from 'next/router'
 
 export const INITIAL_LAT_LONG = [52.455, 5.69306]
 
@@ -13,8 +14,60 @@ const MAP_STYLE = 'mapbox://styles/stevejonk/clo6yz6p200u601qs0wct801b'
 export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
   const data = archeomaps as unknown as Archeomaps
   const mapRef = useRef<MapRef | null>(null)
+  const router = useRouter()
 
   const [[latitude, longitude], setLatLong] = useState<number[]>(INITIAL_LAT_LONG)
+
+  const points: Location[] = useMemo(() => {
+    return data.features
+      .map((loc) => {
+        const id = uuidv4()
+
+        if (
+          !loc?.geometry ||
+          !loc?.geometry?.coordinates ||
+          !loc?.geometry?.coordinates.length ||
+          isNaN(loc.geometry.coordinates[0]) ||
+          isNaN(loc.geometry.coordinates[1])
+        ) {
+          return {
+            id,
+            title: null,
+            description: null,
+            longitude: null,
+            latitude: null,
+          }
+        }
+
+        return {
+          id,
+          title: loc.properties.Name,
+          description: loc.properties.description,
+          longitude: loc.geometry.coordinates[0],
+          latitude: loc.geometry.coordinates[1],
+        }
+      })
+      .filter(Boolean)
+  }, [data])
+
+  useEffect(() => {
+    const locationLatLong = (router.query.location as string)?.split(',')
+    console.log({ locationLatLong })
+
+    if (locationLatLong && locationLatLong.length > 0 && points.length) {
+      const initialLocation = points.find(
+        (loc) =>
+          loc.latitude === Number(locationLatLong[0]) &&
+          loc.longitude === Number(locationLatLong[1])
+      )
+
+      console.log({ initialLocation, points })
+
+      if (initialLocation) {
+        setCurrentLocation(initialLocation)
+      }
+    }
+  }, [router.query.location])
 
   useEffect(() => {
     const toZoom = currentLocation?.id ? 6 : 3
@@ -26,39 +79,17 @@ export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
     })
   }, [currentLocation, latitude, longitude])
 
-  const points: Location[] = useMemo(() => {
-    return data.features
-      .map((loc) => {
-        if (
-          !loc?.geometry ||
-          !loc?.geometry?.coordinates ||
-          !loc?.geometry?.coordinates.length ||
-          isNaN(loc.geometry.coordinates[0]) ||
-          isNaN(loc.geometry.coordinates[1])
-        ) {
-          return { longitude: null, latitude: null }
-        }
-
-        return {
-          id: uuidv4(),
-          title: loc.properties.name,
-          longitude: loc.geometry.coordinates[0],
-          latitude: loc.geometry.coordinates[1],
-        }
-      })
-      .filter(Boolean) as Location[]
-  }, [data])
-
   function onLocationDetail(setLoc: Location) {
     setCurrentLocation(setLoc)
     setLatLong([setLoc.longitude, setLoc.longitude])
+    router.replace(`/?location=${setLoc.latitude},${setLoc.longitude}`)
   }
 
   return (
     <>
       <AnimatePresence>
         {currentLocation && (
-          <DetailView title={currentLocation.title} description={currentLocation.title} />
+          <DetailView title={currentLocation.title} description={currentLocation.description} />
         )}
       </AnimatePresence>
 
@@ -74,8 +105,6 @@ export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       >
         {points.map((location) => {
-          if (!location) return null
-
           return (
             <Marker
               key={location.id}
@@ -99,9 +128,9 @@ type Feature = {
   type: 'Feature'
   geometry: {
     type: 'Point'
-    coordinates: [-64.6875, -71.746432]
+    coordinates: [number, number]
   }
-  properties: Record<string, null | string | number>
+  properties: Record<string, null | string>
 }
 
 type Archeomaps = {
@@ -112,7 +141,8 @@ type Archeomaps = {
 
 export type Location = {
   id: string
-  title: string
-  longitude: number
-  latitude: number
+  title: string | null
+  description: string | null
+  longitude: number | null
+  latitude: number | null
 }
