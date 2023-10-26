0031606cc6867155ba517e06fcd645885f77c7b0
Ronny Rook <info@ronnyrook.nl>
2023-10-26 14:29:54 +0200

    Initial loading location

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import { Map, MapRef, Marker, Layer, Source, MapLayerMouseEvent, GeoJSONSource } from 'react-map-gl'
+import { useEffect, useRef } from 'react'
+import { Map, MapRef, Layer, Source, MapLayerMouseEvent, GeoJSONSource } from 'react-map-gl'
 import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
-import { v4 as uuidv4 } from 'uuid'
 import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './layers'
 
 import { SetLocationProps } from 'pages'
 import archeomaps from 'data/archeomaps.json'
-// import archeomapsGeo from 'data/archeomaps.geojson'
 
 import { DetailView } from './DetailView'
 
@@ -20,111 +18,46 @@ export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
   const mapRef = useRef<MapRef | null>(null)
   const router = useRouter()
 
-  // const [[latitude, longitude], setLatLong] = useState<number[]>(INITIAL_LAT_LONG)
-
-  // const points: Location[] = useMemo(() => {
-  //   return data.features
-  //     .map((loc) => {
-  //       const id = uuidv4()
-
-  //       if (
-  //         !loc?.geometry ||
-  //         !loc?.geometry?.coordinates ||
-  //         !loc?.geometry?.coordinates.length ||
-  //         isNaN(loc.geometry.coordinates[0]) ||
-  //         isNaN(loc.geometry.coordinates[1])
-  //       ) {
-  //         return {
-  //           id,
-  //           title: null,
-  //           description: null,
-  //           longitude: null,
-  //           latitude: null,
-  //         }
-  //       }
-  //       return {
-  //         id,
-  //         title: loc.properties.name,
-  //         description: loc.properties.description,
-  //         longitude: loc.geometry.coordinates[0],
-  //         latitude: loc.geometry.coordinates[1],
-  //       }
-  //     })
-  //     .filter(Boolean)
-  // }, [data])
-
-  // useEffect(() => {
-  //   const locationLatLong = (router.query.location as string)?.split(',')
-
-  //   if (locationLatLong && locationLatLong.length > 0 && points.length) {
-  //     const initialLocation = points.find(
-  //       (loc) =>
-  //         loc.latitude === Number(locationLatLong[0]) &&
-  //         loc.longitude === Number(locationLatLong[1])
-  //     )
-
-  //     if (initialLocation) {
-  //       setCurrentLocation(initialLocation)
-  //     }
-  //   }
-  // }, [router.query.location])
-
-  // useEffect(() => {
-  //   const toZoom = currentLocation?.id ? 6 : 3
-
-  //   mapRef.current?.flyTo({
-  //     center: [longitude, latitude],
-  //     zoom: toZoom, // reset to initial zoom when going back to overview
-  //     duration: 2000,
-  //   })
-  // }, [currentLocation, latitude, longitude])
-
-  // function onLocationDetail(setLoc: Location) {
-  //   setCurrentLocation(setLoc)
-  //   setLatLong([setLoc.longitude, setLoc.longitude])
-  //   router.replace(`/?location=${setLoc.latitude},${setLoc.longitude}`)
-  // }
-
   function onMapClick(event: MapLayerMouseEvent) {
-    console.log({
-      event,
-    })
-
     const features = event.features || []
 
     if (features.length > 0) {
-      console.log({ features })
+      const feature = event.features[0] as GeoJSON.Feature<GeoJSON.Point>
 
-      const feature = event.features[0]
       const clusterId = feature.properties.cluster_id
-
       const mapboxSource = mapRef.current.getSource('archeomaps') as GeoJSONSource
 
-      mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
-        if (err) {
-          return
-        }
+      if (clusterId) {
+        mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return
 
-        mapRef.current.easeTo({
-          center: feature.geometry.coordinates,
-          zoom,
-          duration: 500,
+          mapRef.current.easeTo({
+            center: feature.geometry.coordinates as [number, number],
+            zoom,
+            duration: 500,
+          })
         })
-      })
+      }
+
+      if (!clusterId && feature) {
+        const long = feature.geometry.coordinates[0]
+        const lat = feature.geometry.coordinates[1]
+
+        setCurrentLocation({
+          name: feature.properties.name,
+          description: feature.properties.description,
+          longitude: long,
+          latitude: lat,
+        })
+      }
     }
   }
-
-  // function onLocationDetail(setLoc: Location) {
-  //   setCurrentLocation(setLoc)
-  //   setLatLong([setLoc.latitude, setLoc.longitude])
-  //   router.replace(`/?location=${setLoc.latitude},${setLoc.longitude}`)
-  // }
 
   return (
     <>
       <AnimatePresence>
         {currentLocation && (
-          <DetailView title={currentLocation.title} description={currentLocation.description} />
+          <DetailView title={currentLocation.name} description={currentLocation.description} />
         )}
       </AnimatePresence>
 
@@ -140,7 +73,6 @@ export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
         interactiveLayerIds={[clusterLayer.id, unclusteredPointLayer.id]}
         onClick={onMapClick}
-        // onMouseOver={(event) => {}
       >
         <Source
           id="archeomaps"
@@ -154,20 +86,6 @@ export function MapView({ currentLocation, setCurrentLocation }: MapViewProps) {
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
         </Source>
-
-        {/* {points.map((location, index) => {
-          return (
-            <Marker
-              key={`${index}-${location.id}`}
-              longitude={location.longitude}
-              latitude={location.latitude}
-              style={{ position: 'absolute', left: 0, top: 0, cursor: 'pointer' }}
-              onClick={() => onLocationDetail(location)}
-            >
-              <div className="h-2 w-2 rounded-lg bg-gray-200 hover:bg-gray-700" />
-            </Marker>
-          )
-        })} */}
       </Map>
     </>
   )
@@ -178,8 +96,7 @@ type MapViewProps = SetLocationProps
 type Archeomaps = GeoJSON.FeatureCollection<GeoJSON.Geometry>
 
 export type Location = {
-  id: string
-  title: string | null
+  name: string | null
   description: string | null
   longitude: number | null
   latitude: number | null
